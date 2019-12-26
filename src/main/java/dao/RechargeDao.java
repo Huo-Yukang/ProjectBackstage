8ffcb490445fe63f5c5a2d878dc714ebaccd5b66
Huo-Yukang <1939449803@qq.com>
2019-12-26 12:33:50 +0800

    gy

--- src/main/java/dao/RechargeDao.java
+++ src/main/java/dao/RechargeDao.java
@@ -23,13 +23,24 @@ public class RechargeDao {
     public User recharge(User user) throws SQLException {
         PreparedStatement preparedStatement = null;
         Connection connection = JdbcHelper.getConn();
-        System.out.println(user.getBalance());
+        preparedStatement = connection.prepareStatement("select  * from user where id=?");
+        preparedStatement.setInt(1,user.getId());
+        ResultSet resultSet0 = preparedStatement.executeQuery();
+        User user0 = null;
+        if (resultSet0.next()){
+            user0 = new User(resultSet0.getInt("id"),
+                    resultSet0.getString("username"),
+                    resultSet0.getString("password"),
+                    resultSet0.getString("call_phone"),
+                    resultSet0.getString("address"),
+                    resultSet0.getInt("balance"));
+        }
         preparedStatement = connection.prepareStatement("update user set balance=? where id=?");
-        preparedStatement.setInt(1,user.getBalance() + 500);
-        preparedStatement.setInt(2,user.getId());
+        preparedStatement.setInt(1,user0.getBalance() + 500);
+        preparedStatement.setInt(2,user0.getId());
         preparedStatement.executeUpdate();
         preparedStatement = connection.prepareStatement("select * from user where id =?");
-        preparedStatement.setInt(1,user.getId());
+        preparedStatement.setInt(1,user0.getId());
         ResultSet resultSet = preparedStatement.executeQuery();
         User user1 = null;
         if (resultSet.next()){
